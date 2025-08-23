7fa7bd654d7429128413341d0186d9529d609a00
ANONYMOUS <ANONYMOUS>
2025-08-23 12:50:10 +0100

    upgrade

--- build.sh
+++ build.sh
@@ -3,12 +3,23 @@
 set -o errexit
 
 # Install system dependencies
-apt-get update
-apt-get install -y python3-dev libpq-dev
+apt-get update && apt-get install -y \
+    python3-dev \
+    libpq-dev \
+    gcc \
+    postgresql \
+    postgresql-contrib
 
-# Install Python dependencies
+# Upgrade pip and install psycopg2 first
 pip install --upgrade pip
+pip install psycopg2-binary==2.9.9
+
+# Then install other requirements
 pip install -r requirements.txt
+
+# Clear pyc files
+find . -type f -name "*.pyc" -delete
+find . -type d -name "__pycache__" -delete
 
 # Run migrations
 python manage.py collectstatic --no-input
